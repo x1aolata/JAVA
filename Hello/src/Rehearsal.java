dae437cbfb653fc50ac0f755ed6f09ece6711f1f
x1aolata <45487873+x1aolata@users.noreply.github.com>
2019-01-27 18:07:28 +0800

    learn

--- Hello/src/Rehearsal.java
+++ Hello/src/Rehearsal.java
@@ -6,8 +6,14 @@ public class Rehearsal {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 
+//		boolean a = true;
+//		double sum=0;
+//		int n = in.nextInt();
+//		for (int i = 1; i <= n; i++) {
+//			sum += (double)( 1.0 / i);
+//		}
+//		System.out.printf("%.2f",sum);
+
 		
-		System.out.println("恭喜你猜中了,数字是：" +"。");
-		System.out.println("你猜了" +"次。");
 	}
 }
